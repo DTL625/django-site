20dc4a8e34db4fa09c68423f16979783344b9f7b
DTL <dtl@dtl.com>
2020-04-29 07:39:47 +0800

    add base

--- blog/urls.py
+++ blog/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     url(r'^comment/(?P<pk>\d+)/remove', views.comment_remove, name='comment_remove'),
     url(r'^drafts/$', views.DraftListView.as_view(), name='post_draft_list'),
 
-    url(r'^accounts/login/$', auth_view.LoginView.as_view(), name='login'),
+    url(r'^accounts/login/$', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^accounts/logout/$', auth_view.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'})
 ]
