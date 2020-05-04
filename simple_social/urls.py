dce2be0e26849d49c8e965ee63701a4d13f33b2f
DTL <dtl@dtl.com>
2020-05-04 19:55:08 +0800

    part3

--- simple_social/urls.py
+++ simple_social/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from simple_social import views
 
 urlpatterns = [
     url(r'^$', views.HomePage.as_view(), name='social_home'),
+    url(r'accounts/', include('social_account.urls', namespace='accounts')),
+    url(r'accounts/', include('django.contrib.auth.urls'))
 ]
